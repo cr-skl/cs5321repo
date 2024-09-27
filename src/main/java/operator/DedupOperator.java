ff4b74c6abe60b84076a32d60e2c844a73c64b58
skldingguagua <57707541+skldingguagua@users.noreply.github.com>
2024-09-27 11:37:08 -0400

    comment all the java files

--- src/main/java/operator/DedupOperator.java
+++ src/main/java/operator/DedupOperator.java
@@ -6,7 +6,21 @@ public class DedupOperator extends Operator {
   private Operator child;
   private Tuple prevTuple;
 
-  /** Resets cursor on the operator to the beginning */
+  public DedupOperator() {
+    prevTuple = null;
+  }
+
+  /**
+   * set the child, called on .visit()
+   * @param child c
+   */
+  public void setChild(Operator child) {
+    this.child = child;
+  }
+
+  /**
+   * Reset its pointer to null, and the child pointer
+   */
   @Override
   public void reset() {
     child.reset();
@@ -22,18 +36,14 @@ public class DedupOperator extends Operator {
   public Tuple getNextTuple() {
     Tuple nextTuple;
     while ((nextTuple = child.getNextTuple()) != null) {
+      // init or not the same
+      // move prevTuple to nextTuple and return it
       if (prevTuple == null || !nextTuple.equals(prevTuple)) {
         prevTuple = nextTuple;
-        return nextTuple;
+        return prevTuple;
       }
     }
     return null;
   }
-  /**
-   * set the child, called on .visit()
-   * @param child c
-   */
-  public void setChild(Operator child) {
-    this.child = child;
-  }
+
 }

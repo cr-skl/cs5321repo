cc97c465e5633f93bbfa9e73da9cbf6b4e575e78
skldingguagua <57707541+skldingguagua@users.noreply.github.com>
2024-11-11 14:22:13 -0500

    update the TNLJ BNLJ in branch p2_part_3.3

--- src/main/java/visitor/OpVisitor.java
+++ src/main/java/visitor/OpVisitor.java
@@ -1,13 +1,13 @@
-package visitor;
-
-import PhysicalOperator.ProjectOperator;
-import PhysicalOperator.ScanOperator;
-import PhysicalOperator.SelectOperator;
-
-interface OpVisitor {
-  void visit(ScanOperator operator);
-
-  void visit(SelectOperator operator);
-
-  void visit(ProjectOperator operator);
-}
+//package visitor;
+//
+//import PhysicalOperator.ProjectOperator;
+//import PhysicalOperator.ScanOperator;
+//import PhysicalOperator.SelectOperator;
+//
+//interface OpVisitor {
+//  void visit(ScanOperator operator);
+//
+//  void visit(SelectOperator operator);
+//
+//  void visit(ProjectOperator operator);
+//}
